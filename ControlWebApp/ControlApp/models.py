a9d26fec700dd3ad033766ad5c27e87b134946a1
wolfexe777 <wolfexe777@gmail.com>
2023-10-02 21:48:21 +0700

    second iteration

--- ControlWebApp/ControlApp/models.py
+++ ControlWebApp/ControlApp/models.py
@@ -2,8 +2,8 @@ from django.db import models
 from django.utils import timezone
 
 class Post(models.Model):
-    title = models.CharField(max_length=200)
-    content = models.TextField()
+    title = models.CharField(max_length=200, verbose_name='Название')
+    content = models.TextField(verbose_name='Текст публикации')
     date_posted = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
